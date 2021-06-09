3e7b424c40dff7cfd5c6e9e2cc8567d65a289638
Awtanabe <panser098@gmail.com>
2021-06-10 00:09:55 +0900

    dbつくらないでrspecで実行

--- spec/routing/posts_routing_spec.rb
+++ spec/routing/posts_routing_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 RSpec.describe PostsController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/posts').to route_to('posts#index')
+      expect(get: '/posts').t route_to('posts#index')
     end
 
     it 'routes to #new' do
